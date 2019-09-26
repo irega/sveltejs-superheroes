1a0b7c8f72538213a1432302ed72576f75ffe266
irega <ivan.reinoso@outlook.es>
2019-09-26 19:16:54 +0200

    added store subscribe and onDestroy hook

--- src/components/Creation/SuperHeroCreation.svelte
+++ src/components/Creation/SuperHeroCreation.svelte
@@ -1,16 +1,22 @@
 <script>
+  import { onDestroy } from "svelte";
   import store from "../../store.js";
   import SuperHero from "../../models/SuperHero";
 
-  let name;
+  let name, totalSuperHeroes;
 
   function createSuperHero() {
     const superHeroes = $store.superHeroes;
     const lastId = superHeroes[superHeroes.length - 1].id;
     superHeroes.push(new SuperHero(lastId + 1, name, "", true));
     store.setSuperHeroes(superHeroes);
-    store.setRoute('/search');
   }
+
+  const unsubscribe = store.subscribe(value => {
+    totalSuperHeroes = value.superHeroes.length;
+  });
+
+  onDestroy(unsubscribe);
 </script>
 
 <style>
@@ -21,3 +27,4 @@
 <label for="name">Name:</label>
 <input type="text" name="name" bind:value={name} />
 <button on:click={createSuperHero}>Create</button>
+<h2>Total SuperHeroes: {totalSuperHeroes}</h2>
